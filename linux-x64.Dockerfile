7c78a37a5360f1b4e5d67b63d22aaad9e770365c
LongYinan <lynweklm@gmail.com>
2022-07-26 16:06:18 +0800

    ci: build aarch64 gnu Linux

--- linux-x64.Dockerfile
+++ linux-x64.Dockerfile
@@ -1,6 +1,5 @@
 FROM ghcr.io/napi-rs/napi-rs/nodejs-rust:lts-debian
 
 RUN apt-get update && \
-  apt-get install libc++-14-dev ruby -y --fix-missing --no-install-recommends && \
-  ln -sf /usr/lib/llvm-14/lib/libc++abi.so.1.0 /usr/lib/llvm-14/lib/libc++abi.so && \
+  apt-get install libc++-14-dev libc++abi-14-dev ruby -y --fix-missing --no-install-recommends && \
   rm -rf /var/lib/apt/lists/*
--- xtask/src/release_jsc.rs
+++ xtask/src/release_jsc.rs
@@ -5,7 +5,11 @@ use octorust::{auth::Credentials, types::ReposCreateReleaseRequest, Client};
 use serde::Deserialize;
 use tar::{Archive, Builder};
 
-static ICU_DIR_TO_TAR: &[&str] = &["icu/icu4c/include", "icu/icu4c/lib"];
+static ICU_DIR_TO_TAR: &[&str] = &[
+  "icu/icu4c/include",
+  "icu/icu4c/lib",
+  "icu-linux-aarch64/lib",
+];
 
 static WEBKIT_DIR_TO_TAR: &[&str] = &[
   #[cfg(not(target_os = "windows"))]
@@ -25,7 +29,12 @@ static REPO_NAME: &str = "jsc-rs";
 
 #[derive(Debug, Deserialize)]
 struct RootConfig {
-  external: ExternalDependencies,
+  workspace: Workspace,
+}
+
+#[derive(Debug, Deserialize)]
+struct Workspace {
+  metadata: ExternalDependencies,
 }
 
 #[derive(Debug, Deserialize)]
@@ -46,22 +55,28 @@ pub async fn release(target: &str) {
   let mut icu_tar = Builder::new(Vec::new());
   if !cfg!(target_os = "macos") {
     for dir in ICU_DIR_TO_TAR {
-      icu_tar
-        .append_dir_all(dir, current_path.join(dir))
-        .expect(&format!("Append {} failed", dir));
+      let p = current_path.join(dir);
+      if p.exists() {
+        icu_tar
+          .append_dir_all(dir, p)
+          .expect(&format!("Append {} failed", dir));
+      }
     }
   }
   for dir in WEBKIT_DIR_TO_TAR {
-    webkit_tar
-      .append_dir_all(dir, current_path.join(dir))
-      .expect(&format!("Append {} failed", dir));
+    let src_dir = current_path.join(dir);
+    if src_dir.exists() {
+      webkit_tar
+        .append_dir_all(dir, src_dir)
+        .expect(&format!("Append {} failed", dir));
+    }
   }
   let client = create_client();
   let root_config = read_root_config();
   if !cfg!(target_os = "macos") {
     upload_release(
       &client,
-      &root_config.external.icu.tag,
+      &root_config.workspace.metadata.icu.tag,
       target,
       icu_tar.into_inner().expect("Get icu tar failed"),
     )
@@ -69,7 +84,7 @@ pub async fn release(target: &str) {
   }
   upload_release(
     &client,
-    &root_config.external.webkit.tag,
+    &root_config.workspace.metadata.webkit.tag,
     target,
     webkit_tar.into_inner().expect("Get icu tar failed"),
   )
@@ -116,35 +131,43 @@ async fn upload_release(client: &Client, tag: &str, target: &str, tar: Vec<u8>) 
         .expect("Delete release asset failed");
     }
   }
-  reqwest::Client::new()
-    .post(&format!(
-      "https://uploads.github.com/repos/{}/{}/releases/{}/assets?name={}",
-      REPO_OWNER,
-      REPO_NAME,
-      release.id,
-      format!("{}.tar", target)
-    ))
-    .body(tar)
-    .header("Content-Type", "application/x-tar")
-    .header(
-      "Authorization",
-      &format!(
-        "token {}",
-        env::var("GITHUB_TOKEN").expect("No GITHUB_TOKEN provided")
-      ),
-    )
-    .send()
-    .await
-    .expect("Upload release asset failed");
+  let mut ttl = 0;
+  while ttl < 5 {
+    if let Ok(_) = reqwest::Client::new()
+      .post(&format!(
+        "https://uploads.github.com/repos/{}/{}/releases/{}/assets?name={}",
+        REPO_OWNER,
+        REPO_NAME,
+        release.id,
+        format!("{}.tar", target)
+      ))
+      .body(tar.clone())
+      .header("Content-Type", "application/x-tar")
+      .header(
+        "Authorization",
+        &format!(
+          "token {}",
+          env::var("GITHUB_TOKEN").expect("No GITHUB_TOKEN provided")
+        ),
+      )
+      .send()
+      .await
+    {
+      break;
+    } else {
+      ttl += 1;
+    }
+  }
+  assert_ne!(ttl, 5, "Upload release failed");
 }
 
 pub async fn download(target: &str) {
   let root_config = read_root_config();
   let client = create_client();
   if !cfg!(target_os = "macos") {
-    download_and_unpack_asset(&client, &root_config.external.icu.tag, target).await;
+    download_and_unpack_asset(&client, &root_config.workspace.metadata.icu.tag, target).await;
   }
-  download_and_unpack_asset(&client, &root_config.external.webkit.tag, target).await;
+  download_and_unpack_asset(&client, &root_config.workspace.metadata.webkit.tag, target).await;
 }
 
 fn create_client() -> Client {
@@ -179,18 +202,27 @@ async fn download_and_unpack_asset(client: &Client, tag: &str, target: &str) {
     .iter()
     .find(|asset| asset.name == format!("{}.tar", target))
     .expect("Can't find asset");
-  let tar_content = reqwest::ClientBuilder::new()
-    .redirect(reqwest::redirect::Policy::limited(10))
-    .build()
-    .expect("Build reqwest client failed")
-    .get(&asset.browser_download_url)
-    .send()
-    .await
-    .expect("Get asset failed")
-    .bytes()
-    .await
-    .expect("Get asset bytes failed");
-  let mut tar = Archive::new(tar_content.as_ref());
+  let mut ttl = 0;
+  let mut tar_content = None;
+  while ttl < 5 {
+    if let Ok(body) = reqwest::ClientBuilder::new()
+      .redirect(reqwest::redirect::Policy::limited(10))
+      .build()
+      .expect("Build reqwest client failed")
+      .get(&asset.browser_download_url)
+      .send()
+      .await
+      .expect("Get asset failed")
+      .bytes()
+      .await
+    {
+      tar_content = Some(body);
+      break;
+    } else {
+      ttl += 1;
+    }
+  }
+  let mut tar = Archive::new(tar_content.as_ref().unwrap().as_ref());
   tar
     .unpack(env::current_dir().expect("Get current dir failed"))
     .expect("Unpack tar failed");
